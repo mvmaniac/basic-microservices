2546d73d6cf2b97f0370063313eda38942409d9a
yhs <mvmaniaz@gmail.com>
2021-06-16 14:13:03 +0900

    Add: common service

---
+++ service-common/src/main/java/io/devfactory/ServiceCommonApplication.java
@@ -0,0 +1,13 @@
+package io.devfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCommonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceCommonApplication.class, args);
+  }
+
+}

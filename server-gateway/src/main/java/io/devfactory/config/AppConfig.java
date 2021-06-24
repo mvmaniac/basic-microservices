6b181ccf2142478c2a169cc9e02b9abfc38b1390
yhs <mvmaniaz@gmail.com>
2021-06-24 10:21:24 +0900

    [추가] Configuration Service 완료,  Spring Cloud Bus 완료

--- server-gateway/src/main/java/io/devfactory/config/AppConfig.java
+++ server-gateway/src/main/java/io/devfactory/config/AppConfig.java
@@ -1,12 +1,20 @@
 package io.devfactory.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @ConfigurationPropertiesScan("io.devfactory.config")
 @Configuration
 public class AppConfig {
+
+  @Bean
+  public HttpTraceRepository httpTraceRepository() {
+    return new InMemoryHttpTraceRepository();
+  }
 
 }
--- service-member/src/main/java/io/devfactory/web/api/MemberHealthCheckApi.java
+++ service-member/src/main/java/io/devfactory/web/api/MemberHealthCheckApi.java
@@ -2,7 +2,6 @@ package io.devfactory.web.api;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +15,13 @@ public class MemberHealthCheckApi {
 
   @GetMapping("/health-check")
   public String statusA() {
-    return String.format("It's Working in member service on PORT: %s", environment.getProperty("local.server.port"));
+    // environment로 설정 파일 값을 가지고 와야 변경된 설정 파일 값을 동적으로 읽을 수 있을 듯?
+    // 만약 동적으로 값 변경이 필요하다면 @ConfigurationProperties로는 안될 듯?
+    return String.format("It's Working in member service on PORT: %s, server.port: %s, jwt.token.client-secret: %s, jwt.token.expires: %s",
+        environment.getProperty("local.server.port"),
+        environment.getProperty("server.port"),
+        environment.getProperty("jwt.token.client-secret"),
+        environment.getProperty("jwt.token.expires"));
   }
 
 }
